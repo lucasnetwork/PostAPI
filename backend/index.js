8318c73ed80b8ff9df9905e3ec3af35c2e43686f
Lucas Ribeiro <lucasribeiro61374@gmail.com>
2019-09-11 23:32:40 -0300

    adicionando o consign ao index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const consign = require('consign')
 const app = express()
+const DB = require('./config/DB')
 
+app.db = DB
+consign()
+    .then('./api/validation.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 app.listen(3000,()=>{
     console.log("Backend executando")
 })
