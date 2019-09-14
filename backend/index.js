051c17dd2bf65821e4ea430ef5e470872ece7304
Lucas Ribeiro <lucasribeiro61374@gmail.com>
2019-09-13 23:34:46 -0300

    Configurando index

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,12 @@ const consign = require('consign')
 const app = express()
 const DB = require('./config/DB')
 
+
 app.db = DB
 consign()
     .then('./api/validation.js')
     .then('./api')
+    .then('./config/middlewares.js')
     .then('./config/routes.js')
     .into(app)
 app.listen(3000,()=>{
