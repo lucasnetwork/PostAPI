a2869fd00ef96ffe7c96c94d0c23c0fda150d7dc
Lucas Ribeiro <lucasribeiro61374@gmail.com>
2019-09-16 23:27:29 -0300

    Adicionando templates

---
+++ frontend/src/components/template/Header.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import Menu from './Menu'
+
+class Header extends Component{
+    render(){
+        return(
+            <header>menu</header>
+        )
+    }
+}
+
+export default Header
