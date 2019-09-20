67c3ba68d27a9e101f7099927caa4a76b05b4dea
Lucas Ribeiro <lucasribeiro61374@gmail.com>
2019-09-20 09:55:33 -0300

    configurando atualização de posts

--- backend/api/post.js
+++ backend/api/post.js
@@ -2,15 +2,24 @@ module.exports = app =>{
     const {existsOrError} = app.api.validation
     const save = async (req,res) => {
         const post = {...req.body}
+        if(req.params.id) post.id = req.params.id
         try{
             existsOrError(post.name,'Nome não informado')
         }catch(msg){
             return res.status(400).send(msg)
         }
-        app.db('posts')
+        if(!post.id){
+            app.db('posts')
             .insert(post)
             .then(_ => res.status(204).send())
             .catch(err => res.status(500).send(err))
+        }else{
+            app.db('posts')
+            .update(post)
+            .where({id: post.id})
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(500).send(err))
+        }
     }
     const getPosts  = (req,res) =>{
         app.db('posts')
@@ -25,5 +34,15 @@ module.exports = app =>{
             .then(post => res.json(post))
             .catch(err => res.status(500).send(err))
     }
-    return {save,getPosts,getPost}
+    const remove =  async (req,res) =>{
+        try{
+            const rowDeleted = await app.db('posts').where({id:req.params.id}).del()
+            console.log(rowDeleted)
+            existsOrError(rowDeleted,'Post não cadastrado')
+            res.status(204).send()
+        }catch(msg){
+            res.status(500).send(msg)
+        }
+    }
+    return {save,getPosts,getPost,remove}
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -3,5 +3,7 @@ module.exports = app =>{
         .post(app.api.post.save)
         .get(app.api.post.getPosts)
     app.route('/post/:id')
+        .put(app.api.post.save)
         .get(app.api.post.getPost)
+        .delete(app.api.post.remove)
 }
