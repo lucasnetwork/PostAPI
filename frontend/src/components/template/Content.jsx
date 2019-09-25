9c864f3a53201b697db0d196a50ca012238e733e
Lucas Ribeiro <lucasribeiro61374@gmail.com>
2019-09-25 00:02:25 -0300

    criando primeira requisição com axios

--- frontend/src/components/template/Content.jsx
+++ frontend/src/components/template/Content.jsx
@@ -1,11 +1,40 @@
 import React,{Component} from 'react'
 import './../../css/content.css'
+import axios from 'axios'
+
+
 class Content extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            posts:[]
+        }
+
+    }
+    getPosts(){
+        axios.get('http://localhost:3000/post')
+            .then(res => {
+                const posts = res.data.map(post => {
+                    return {...post}
+                })
+                this.setState({posts})
+                console.log(this.state.posts)
+            })
+    }
+    componentDidMount(){
+        this.getPosts()
+    }
     render(){
         return(
-            <main class="main">
-                fjsdakl
+            <main className="content">
+                {this.state.posts.map((post,id) => {
+                    return(<li key={post.id}>
+                            {post.name}
+                        </li>
+                    )})
+                }
             </main>
         )
     }
 }
+export default Content
